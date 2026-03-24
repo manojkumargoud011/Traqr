ede0964d4654e3ea0188e0b074e97ec750142795
ManojKumar <manojkumargoud001@gmail.com>
2026-03-24 18:38:07 +0530

    Added mobile features, dashboard improvements, and fixed login issue

--- src/firebase.js
+++ src/firebase.js
@@ -47,3 +47,16 @@ setPersistence(auth, browserLocalPersistence);
 googleProvider.setCustomParameters({
   prompt: 'select_account'
 });
+
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
+
+// export const app = initializeApp(firebaseConfig);
